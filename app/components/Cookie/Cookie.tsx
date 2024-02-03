7136183f194c044c1e6e02b40e3768c1fcbe680a
Oliver Cook <olliewatford@gmail.com>
2024-02-03 18:00:38 +0000

    Finished

--- app/components/Cookie/Cookie.tsx
+++ app/components/Cookie/Cookie.tsx
@@ -1,15 +1,27 @@
 'use client'
 
+import { useState } from 'react'
+
 interface CookieProps {
   setCount: () => void
 }
 
 export default function Cookie(props: CookieProps) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+    setTimeout(() => setIsClicked(false), 100);
+    props.setCount();
+  }
+
   return (
     <div className="w-1/2 flex justify-center">
-      <button onClick = {props.setCount}>
-        <img src="/cookie.png" alt="cookie" />
+      <button onClick = {handleClick}>
+        <img src="/cookie.png" className={`w-[512px] transform ${isClicked ? 'scale-105' : ''}`} alt="cookie" />
       </button>
     </div>
   );
 }
+
+//<img src="/cookie.png" className={`w-[512px] transform ${isClicked ? 'scale-110' : ''}`} alt="cookie" />
--- app/components/Counter/Counter.tsx
+++ app/components/Counter/Counter.tsx
@@ -8,9 +8,9 @@ export default function Container(props: ContainerProps) {
   return (
     <div className="flex flex-col justify-end items-center w-1/4">
       <div className="flex items-center h-[5vh]">
-        <p className="text-center">1,000,000</p>
+        <p className="text-center text-white font-bold">1,000,000</p>
       </div>
-      <div className="h-[90vh] w-16 sm:w-32 border border-black relative">
+      <div className="h-[90vh] w-16 sm:w-32 border-2 border-white relative">
         <div 
           className="h-36 w-full bg-green-400 absolute bottom-0"
           style={{height: `${(props.currentCount/1000000)*90}vh`}}
@@ -18,7 +18,7 @@ export default function Container(props: ContainerProps) {
         </div>
       </div>
       <div className="flex items-center h-[5vh]">
-        <p className="text-center">{props.currentCount}</p>
+        <p className="text-center text-white font-bold">{props.currentCount}</p>
       </div>
     </div>
   );
