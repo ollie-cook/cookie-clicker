12c29af15d9a6d302163b0750e50c2d2da5cdf26
Oliver Cook <olliewatford@gmail.com>
2024-02-03 18:25:02 +0000

    Made better on iphone

--- app/components/Counter/Counter.tsx
+++ app/components/Counter/Counter.tsx
@@ -7,17 +7,17 @@ interface ContainerProps {
 export default function Container(props: ContainerProps) {
   return (
     <div className="flex flex-col justify-end items-center w-1/4">
-      <div className="flex items-center h-[5vh]">
+      <div className="flex items-center h-[5svh]">
         <p className="text-center text-white font-bold">1,000,000</p>
       </div>
-      <div className="h-[90vh] w-16 sm:w-32 border-2 border-white relative">
+      <div className="h-[90svh] w-16 sm:w-32 border-2 border-white relative">
         <div 
           className="h-36 w-full bg-green-400 absolute bottom-0"
-          style={{height: `${(props.currentCount/1000000)*90}vh`}}
+          style={{height: `${(props.currentCount/1000000)*90}svh`}}
         >
         </div>
       </div>
-      <div className="flex items-center h-[5vh]">
+      <div className="flex items-center h-[5svh]">
         <p className="text-center text-white font-bold">{props.currentCount}</p>
       </div>
     </div>
