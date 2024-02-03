07a30d5e93b1231fbef18c8c5e1cd0cd7116107d
Oliver Cook <olliewatford@gmail.com>
2024-02-03 17:49:46 +0000

    Implemented cookies

--- app/components/Container/Container.tsx
+++ app/components/Container/Container.tsx
@@ -1,16 +1,27 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Cookie from '../Cookie'
 import Counter from '../Counter'
+import Cookies from 'js-cookie'
 
 export default function Container() {
   const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    if (Cookies.get('cookie-count') !== undefined) {
+      setCount(parseInt(Cookies.get('cookie-count') || "0"))
+    } 
+  }, [])
+
+  const updateCount = () => {
+    setCount(prev => prev+1)
+    Cookies.set('cookie-count', count.toString())
+  }
 
   return (
     <div className="absolute top-0 bottom-0 right-0 left-0 flex justify-end h-full">
-      <Cookie setCount={() => setCount(prev => prev+1)}/>
+      <Cookie setCount={updateCount}/>
       <Counter currentCount = {count}/>
     </div>
   );
